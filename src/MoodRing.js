be0e008eae39143885f45995296dc47f57a645c2
Dev Jana <devjana@Devs-MBP-2.domain>
2020-04-28 12:50:13 -0500

    end of lecture 1

--- src/MoodRing.js
+++ src/MoodRing.js
@@ -10,32 +10,21 @@ class MoodRing extends React.Component{
     componentDidMount(){
         console.log( 'MoodRing mounted. Mood at component did mount:', this.state.mood, this.state.color );
     } // end func
-    // run when the user selects a new color
-    handleColorChange = ( event ) =>{
-        // get the newly selected color
-        console.log( 'in handleColorChange:', event.target.value );
-        // MUST use setState (don't "mutate state directly")
+    // run when the user changes a property
+    handleChangeFor = ( event, property )=>{
+        console.log( 'in handleChangeFor:', property, event.target.value );
         this.setState({
-            color: event.target.value
-        })
-    }
-
-    handleMoodChange = ( event ) =>{
-        console.log( 'in handleMoodChange:', event.target.value );
-        this.setState(
-            {
-                mood: event.target.value
-            }
-        ) // end set state
-    } // end func
+            [property]: event.target.value
+        }) // end setState
+    } // end handleChangeFor
 
     render(){
         return(
             <div>
                 {/* comments work in here... */}
                 <p>Some mood ring stuff. Set color and mood. Then display on DOM</p>
-                <input type="text" placeholder="mood?" onChange={ this.handleMoodChange }></input>
-                <select onChange={ this.handleColorChange }>
+                <input type="text" placeholder="mood?" onChange={ ( event )=>this.handleChangeFor( event, "mood" ) }></input>
+                <select onChange={ ( event )=>this.handleChangeFor( event, "color" ) }>
                     <option>Red</option>
                     <option>Orange</option>
                     <option>Yellow</option>
